c11a769c25fc2301c60dadf58eb35209bda3b827
Moideen Nazaif VM <nazaifmoid@gmail.com>
2019-05-16 15:37:28 +0530

    pb2 solved

---
+++ pb2/sol.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+
+
+guard let ndTemp = readLine() else { fatalError("Bad input") }
+let nd = ndTemp.split(separator: " ").map{ String($0) }
+
+guard let n = Int(nd[0].trimmingCharacters(in: .whitespacesAndNewlines))
+else { fatalError("Bad input") }
+
+guard let d = Int(nd[1].trimmingCharacters(in: .whitespacesAndNewlines))
+else { fatalError("Bad input") }
+
+guard let aTemp = readLine() else { fatalError("Bad input") }
+var a: [Int] = aTemp.split(separator: " ").map {
+    if let aItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+        return aItem
+    } else { fatalError("Bad input") }
+}
+
+guard a.count == n else { fatalError("Bad input") }
+
+for i in 0 ..< d {
+    a.append(a[0])
+    a.remove(at: 0)
+}
+
+for i in 0 ..< n {
+    print("\(a[i])",terminator: " ")
+}
