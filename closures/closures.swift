295ca7f5b5bd330d8208b84cbcf41402363ada6a
Moideen Nazaif VM <nazaifmoid@gmail.com>
2020-01-26 19:00:00 +0530

    closures modification

--- closures/closures.swift
+++ closures/closures.swift
@@ -2,9 +2,9 @@ import Foundation
 
 func getNumbersWithPredicate(condition: (Int) -> Bool, numbers: [Int]) -> [Int] {
     var result = [Int]()
-    for num in numbers {
-        if condition(num) {
-            result.append(num)
+    numbers.forEach {
+        if condition($0) {
+            result.append($0)
         }
     }
     return result
@@ -15,16 +15,16 @@ let filteredValues = getNumbersWithPredicate(condition: { (value) -> Bool in
 }, numbers: [34,2,7,15,14])
 print(filteredValues)
 
-func greaterThan5(_ value: Int) -> Bool {
+func greaterThan5(value: Int) -> Bool {
     return value > 5
 }
 
-func isEven(_ value: Int) -> Bool {
+func isEven(value: Int) -> Bool {
     return value.isMultiple(of: 2)
 }
 
-let greaterThan5Values = getNumbersWithPredicate(condition: greaterThan5(_:), numbers: [3,5,6,7])
+let greaterThan5Values = getNumbersWithPredicate(condition: greaterThan5, numbers: [3,5,6,7])
 print(greaterThan5Values)
 
-let evenValues = getNumbersWithPredicate(condition: isEven(_:), numbers: [0,1,2,3,4,5,6,7,8,9,10,99])
+let evenValues = getNumbersWithPredicate(condition: isEven, numbers: [0,1,2,3,4,5,6,7,8,9,10,99])
 print(evenValues)
