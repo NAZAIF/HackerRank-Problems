954bf09e16aec0d01fbd034610c4fc092e10722e
Moideen Nazaif VM <nazaifmoid@gmail.com>
2020-01-27 20:32:54 +0530

    Grading Problem

---
+++ pb4/grading.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+func gradingStudents(grades: [Int]) -> [Int] {
+    // Write your code here
+    var roundedValues = [Int]()
+    for grade in grades {
+        if grade > 37, !grade.isMultiple(of: 5) {
+            let div: Int = grade/5
+            if ((div+1)*5) - grade < 3 {
+                roundedValues.append((div+1)*5)
+            } else {
+                roundedValues.append(grade)
+            }
+        } else {
+            roundedValues.append(grade)
+        }
+    }
+    
+    return roundedValues
+}
+
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let gradesCount = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+var grades = [Int]()
+
+for _ in 1...gradesCount {
+    guard let gradesItem = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+    else { fatalError("Bad input") }
+
+    grades.append(gradesItem)
+}
+
+guard grades.count == gradesCount else { fatalError("Bad input") }
+
+let result = gradingStudents(grades: grades)
+
+fileHandle.write(result.map{ String($0) }.joined(separator: "\n").data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
+
